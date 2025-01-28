a15673c8d7549be5e52d477970b52414d348d040
mba58 <161256536+mba58@users.noreply.github.com>
2025-01-28 11:30:38 +0100

    LP36131: Unterscheidung CSV und DTA; Umbenennungen.

---
+++ DMAS.AEC/40.DMAS_AEC.Umsetzung/Upload/aoec.hs92.product_country2.upload.csv.sql
@@ -0,0 +1,15 @@
+use role sysadmin;
+use database DMAS;
+use schema AOEC;
+
+use warehouse DMAS_2X;
+alter warehouse DMAS_2X resume;
+
+put file://c:/Users/balzer/Documents/Projekte/Sulzer/UserStories_Sulzer/DMAS/DMAS.AEC/10.DMAS_AEC.Materialien/Dataset.HS_92/hs92_country_country_product\hs92_country_country_product_year_1.csv
+	@AOEC_FILES_HS92
+;
+put file://c:/Users/balzer/Documents/Projekte/Sulzer/UserStories_Sulzer/DMAS/DMAS.AEC/10.DMAS_AEC.Materialien/Dataset.HS_92/hs92_country_country_product\hs92_country_country_product_year_6_1995_1999.csv
+	@AOEC_FILES_HS92
+;
+
+alter warehouse DMAS_2X suspend;
